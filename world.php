a139c72d787e9d4616e8f83fb2f6ebf2c66eff1e
najeeb hall <naj404@hotmail.com>
2017-11-16 20:42:39 +0000

    final

--- world.php
+++ world.php
@@ -1,5 +1,6 @@
 <?php
 $country = $_GET['country'];
+$all = $_GET['all'];
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
@@ -7,11 +8,30 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
+$stmt_all = $conn->query("SELECT * FROM countries");
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+if ($all == true)
+{
+    $results = $stmt_all->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) 
+    {
+      echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
 }
-echo '</ul>';
+if ($country == NULL && $all == false )
+{
+  echo 'Nothing Entered';
+  
+}
+else{
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
