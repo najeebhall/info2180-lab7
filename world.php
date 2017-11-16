d6b6d4cb223c30a3db64fa401e7bb156103cbcea
najeeb hall <naj404@hotmail.com>
2017-11-16 19:51:37 +0000

    excercise 3 complete

--- world.php
+++ world.php
@@ -1,5 +1,5 @@
 <?php
-
+$country = $_GET['country'];
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
@@ -7,10 +7,9 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 echo '<ul>';
 foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
