7db168a1369f3db62fad4c0adce17eca5fbea78c
najeeb hall <naj404@hotmail.com>
2017-11-16 20:07:58 +0000

    excercise 3 complete

--- world.js
+++ world.js
@@ -1,19 +1,30 @@
-window.onload = function(){
+//window.onload = function(){
+
+$(document).ready(function(){    
     var find = document.getElementById("lookup");
+    var lookup = $("#lookup");
     find.onclick=function() {
-       /* $.ajax({
+       $.ajax({
             type: "GET",
             url: "world.php",
             datatype: "html",
-        })*/
-        var data = document.getElementById("country").value;
+            data: {
+                    country: $("#country").val()
+                },
+                success: function(text){
+                    $("#result").html(text);
+                }
+        })
+    }
+    })
+       /* var data = document.getElementById("country").value;
         var httpRequest = new XMLHttpRequest();
         httpRequest.onreadystatechange = function(){
         if (httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200) {
                 document.getElementById("result").innerHTML= httpRequest.responseText;
             }
         };
-        httpRequest.open('GET', "world.php?q="+data,true);
-        httpRequest.send();
-    }
-}
+        httpRequest.open('GET', "world.php?country="+data,true);
+        httpRequest.send();*/
+   // }
+//}
